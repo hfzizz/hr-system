6615562e49892eacdce71b9bc212f2c3b0dfb8f9
Hafiz Izzuddin <110881644+hfzizz@users.noreply.github.com>
2025-02-11 14:55:32 +0800

    remove conflict marker

--- components/apps.py
+++ components/apps.py
@@ -1,12 +1,6 @@
 from django.apps import AppConfig
 
 
-<<<<<<<< HEAD:components/apps.py
 class ComponentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'components'
-========
-class DynamicTablesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dynamic_tables'
->>>>>>>> 1afc9bba66615380003bbbac2b1f2f322bc00c44:dynamic_tables/apps.py
